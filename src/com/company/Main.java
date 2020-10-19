1263d964740e001e18bea00bbe0620407fdb02b6
Талгат Ракишев <>
2020-10-20 00:05:14 +0600

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,10 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        List<String> fileList = new ArrayList<>();
-        String outputPath = "";
-        for(int i = 0; i < args.length - 1; i++){
-            fileList.add(args[i]);
-        }
-        outputPath = args[args.length - 1];
-        new ThreadsStarter().start(fileList, outputPath, ";");
+        FileCollector fc = new FileCollector();
+        fc.addFiles(args);
+        new ThreadsStarter().start(fc.getFileList(), fc.getOutputPath(), ";");
     }
 }
